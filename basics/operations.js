01f586df47f62be098c7153260154fef7406d483
Sahil-Ambigar <138796285+Sahil-Ambigar@users.noreply.github.com>
2025-04-29 13:52:45 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+console.log(2+2);
+console.log("2" + 2);
+
+//written in book
+
