c90c91d1ce341049c35d9e142d1ce0ef9e4d877e
Sahil-Ambigar <138796285+Sahil-Ambigar@users.noreply.github.com>
2025-06-01 07:00:38 +0000

    arrays

---
+++ practice/array.js
@@ -0,0 +1,13 @@
+let fruits=["apple","banana","orange"]
+console.log(fruits[0]);
+
+// for loop array
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// for...of
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
